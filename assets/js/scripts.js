408d085ad99e057720d31e75279da7fd238d3749
mike-mendoza <mendozalugo@gmail.com>
2024-02-26 22:33:11 +0100

    update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -20,7 +20,7 @@ $('.parallax-spacer').parallax({
 
 // Counting down:
 // Set the date we're counting down to
-var countDownDate = new Date(dateCountDown).getTime();
+var countDownDate = new Date("August 10, 2024 13:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
